c4f6e3abf11a352ee566aadd20a8f982bfb6f0e1
minwoo129 <key0129mw@naver.com>
2025-07-02 11:26:59 +0900

    feat: #6 useInput 훅 수정
    - 반환하는 메서드 이름 수정

--- src/hooks/useInput.tsx
+++ src/hooks/useInput.tsx
@@ -20,12 +20,12 @@ import { useCallback, useMemo, useState } from 'react';
 export function useInput<T>(initialValue: T): [T, (value: T) => void] {
   const [value, setValue] = useState<T>(initialValue);
 
-  const handleValueChange = useCallback(
+  const handleChangeValue = useCallback(
     (value: T) => {
       setValue(value);
     },
     [setValue],
   );
 
-  return useMemo(() => [value, handleValueChange], [value, handleValueChange]);
+  return useMemo(() => [value, handleChangeValue], [value, handleChangeValue]);
 }
