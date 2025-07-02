e9ea99616f3f2d3ec23fbc6c5874f6323b5ff670
minwoo129 <key0129mw@naver.com>
2025-07-02 10:25:48 +0900

    feat: #4 eslint, prettier 컨벤션 설정

--- eslint.config.js
+++ eslint.config.js
@@ -1,9 +1,11 @@
-import js from '@eslint/js'
-import globals from 'globals'
-import reactHooks from 'eslint-plugin-react-hooks'
-import reactRefresh from 'eslint-plugin-react-refresh'
-import tseslint from 'typescript-eslint'
-import { globalIgnores } from 'eslint/config'
+import js from '@eslint/js';
+import globals from 'globals';
+import reactHooks from 'eslint-plugin-react-hooks';
+import reactRefresh from 'eslint-plugin-react-refresh';
+import tseslint from 'typescript-eslint';
+import { globalIgnores } from 'eslint/config';
+import pluginReact from 'eslint-plugin-react';
+import pluginPrettier from 'eslint-plugin-prettier';
 
 export default tseslint.config([
   globalIgnores(['dist']),
@@ -15,9 +17,22 @@ export default tseslint.config([
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
     ],
+    plugins: {
+      js,
+      react: pluginReact,
+      prettier: pluginPrettier,
+      '@typescript-eslint': tseslint.plugin,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      'prettier/prettier': 'error',
+      'no-unused-vars': 'error',
+      '@typescript-eslint/no-unused-vars': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'no-var': 'error',
+    },
   },
-])
+]);
