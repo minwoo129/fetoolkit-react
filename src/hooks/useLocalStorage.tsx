24e4c467080a822e54a32587d35c54679f792eb1
minwoo129 <key0129mw@naver.com>
2025-07-02 14:40:25 +0900

    feat: #6 useLocalStorage, useSessionStorage 설명 주석 추가

--- src/hooks/useLocalStorage.tsx
+++ src/hooks/useLocalStorage.tsx
@@ -8,6 +8,27 @@ type ReturnType = [
   () => void,
 ];
 
+/**
+ * ======================= useLocalStorage Hook =======================
+ * - Description
+ *    - 브라우저의 `localStorage`를 사용하여 데이터를 저장하고 관리하기 위한 커스텀 훅입니다.
+ *    - get
+ *      - 특정 키에 저장된 데이터를 가져오는 메서드입니다. 데이터가 없으면 `null`을 반환합니다.
+ *      - 제네릭 타입 선언이 필요합니다.
+ *    - set
+ *      - 특정 키에 데이터를 저장하는 메서드입니다. 데이터를 JSON 문자열로 변환하여 저장합니다.
+ *      - 제네릭 타입 선언이 필요합니다.
+ *    - remove
+ *      - 특정 키에 저장된 데이터를 삭제하는 메서드입니다.
+ *    - clear
+ *      - `localStorage`에 저장된 모든 데이터를 삭제하는 메서드입니다.
+ * - 사용 예시
+ *    ```tsx
+ *    import { useLocalStorage } from '@fetoolkit/react/hooks';
+ *    ...
+ *    const [get, set, remove, clear] = useLocalStorage();
+ *    ```
+ */
 export function useLocalStorage(): ReturnType {
   const get = useCallback(<T,>(key: string) => {
     const response = localStorage.getItem(key);
--- src/hooks/useSessionStorage.tsx
+++ src/hooks/useSessionStorage.tsx
@@ -7,6 +7,28 @@ type ReturnType = [
   (key: string) => void,
   () => void,
 ];
+
+/**
+ * ======================= useSessionStorage Hook =======================
+ * - Description
+ *    - 브라우저의 `sessionStorage`를 사용하여 데이터를 저장하고 관리하기 위한 커스텀 훅입니다.
+ *    - get
+ *      - 특정 키에 저장된 데이터를 가져오는 메서드입니다. 데이터가 없으면 `null`을 반환합니다.
+ *      - 제네릭 타입 선언이 필요합니다.
+ *    - set
+ *      - 특정 키에 데이터를 저장하는 메서드입니다. 데이터를 JSON 문자열로 변환하여 저장합니다.
+ *      - 제네릭 타입 선언이 필요합니다.
+ *    - remove
+ *      - 특정 키에 저장된 데이터를 삭제하는 메서드입니다.
+ *    - clear
+ *      - `localStorage`에 저장된 모든 데이터를 삭제하는 메서드입니다.
+ * - 사용 예시
+ *    ```tsx
+ *    import { useSessionStorage } from '@fetoolkit/react/hooks';
+ *    ...
+ *    const [get, set, remove, clear] = useSessionStorage();
+ *    ```
+ */
 export function useSessionStorage(): ReturnType {
   const get = useCallback(<T,>(key: string) => {
     const response = window.sessionStorage.getItem(key);
