56ddb6a3a4381f8d2891e53aac04cd0fe0af2323
minwoo129 <key0129mw@naver.com>
2025-07-02 11:24:00 +0900

    feat: #6 useInput 훅 사용 문서 작성

--- src/hooks/useInput.tsx
+++ src/hooks/useInput.tsx
@@ -1,6 +1,22 @@
 /* eslint-disable no-unused-vars */
 import { useCallback, useMemo, useState } from 'react';
 
+/**
+ * ======================= useInput Hook =======================
+ * - Description
+ *     - 화면 내 input 요소의 상태를 관리하기 위한 커스텀 훅입니다.
+ *     - 이 훅은 초기값을 받아 해당 값을 상태로 관리하며, 현재 상태값과 상태값 변경 메서드를 제공하는 튜플을 반환합니다.
+ * - 사용 예시 1
+ *     ```tsx
+ *       import { useInput } from '@fetoolkit/react/hooks';
+ *
+ *       ...
+ *
+ *       const [name, handleChangeName] = useInput(''); // 타입추론이 유도된 경우입니다.
+ *       const [age, handleChangeAge] = useInput<number>(0); // 타입을 명시적으로 지정한 경우입니다.
+ *     ```
+ *
+ */
 export function useInput<T>(initialValue: T): [T, (value: T) => void] {
   const [value, setValue] = useState<T>(initialValue);
 
