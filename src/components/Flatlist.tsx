ccb2291c0ee7250e817734561b592cdd4d05479c
minwoo129 <key0129mw@naver.com>
2025-07-03 21:05:30 +0900

    feat: #8 Flatlist 컴포넌트 props JSDOC 주석 추가

--- src/components/Flatlist.tsx
+++ src/components/Flatlist.tsx
@@ -12,11 +12,24 @@ interface Props<T>
     ComponentProps<typeof InfiniteScroll>,
     'children' | 'dataLength'
   > {
+  /** 데이터 목록 */
   datas: T[];
+  /** 렌더링해서 보여줄 아이템 컴포넌트 */
   renderItem: RenderItemType<T>;
+  /**
+   * key props에 적용할 key값을 변환하는 함수
+   * - 대용량 데이터를 다루는 것을 기반으로 하기 때문에 단순 index로만 key props를 적용할 수 없음!!!
+   */
   keyExtractor: KeyExtractorType<T>;
+  /**
+   * 아이템 컴포넌트 사이에 보여줄 구분선 컴포넌트
+   */
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   ItemDividerComponent?: ComponentType<any>;
+  /**
+   * 스크롤 영역의 데이터 길이
+   * - default: 0
+   */
   dataLength?: number;
 }
 
