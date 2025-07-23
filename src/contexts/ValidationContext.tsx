f4aa1aac1a839b50ece003e9ff6db9837a469e23
minwoo129 <key0129mw@naver.com>
2025-07-23 10:34:41 +0900

    feat: #34 유효성검사 기능 관련 JSDOC 추가

--- src/contexts/ValidationContext.tsx
+++ src/contexts/ValidationContext.tsx
@@ -6,10 +6,17 @@ export type ValidatorType<K extends string = string> = Record<
 >;
 
 type ValidatorActionType = {
+  /** 유효성검사 메서드 */
   // eslint-disable-next-line no-unused-vars
   validator: (value: string) => boolean;
+  /**
+   * 유효성검사 에러 정보
+   * - 유효성검사 결과 유효하지 않은 경우 함께 노출할 에러코드와 메시지
+   */
   errorStatus: {
+    /** 에러 코드 */
     errorCode: string;
+    /** 에러 메시지 */
     errorMessage: string;
   };
 };
@@ -28,8 +35,11 @@ type ProviderArgs<K extends string = string> = {
 };
 
 export type ValidationStatusType = {
+  /** 유효성검사 통과여부 */
   isPassed: boolean;
+  /** 유효성검사 실패시 에러코드 */
   errorCode?: string;
+  /** 유효성검사 실패시 에러메시지 */
   errorMessage?: string;
 };
 
